fb2f12bfdb3e1fb3c27e89439794155bf6f4518c
elenalinares <elenalinares06@gmail.com>
2025-05-24 16:41:05 +0200

    hair_feature_extractor.py

--- util/hair_feature_extractor.py
+++ util/hair_feature_extractor.py
@@ -23,31 +23,31 @@ def analyze_hair_amount(image, low_threshold_pct=0.5, high_threshold_pct=2.5):
     if image is None:
         return {'hair_level': np.nan, 'hair_coverage_pct': np.nan}
 
-    # 1. Convert image to grayscale, which is required for the black-hat operation.
+    # 1. Convert image to grayscale
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # 2. Perform the black-hat filtering to create a mask of dark, hair-like structures.
-    #    A kernel size of 25x25 is chosen as it's larger than the typical hair width.
+    # 2. Perform black-hat filtering
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
     blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
 
-    # 3. Threshold the black-hat image to create a clean binary mask of the hair.
-    #    A value of 15 is a robust starting point for dermoscopic images.
+    # 3. Threshold the black-hat image
     _, hair_mask = cv2.threshold(blackhat, 15, 255, cv2.THRESH_BINARY)
 
-    # 4. Quantify the hair by calculating its percentage coverage of the total image area.
-    #    This makes the metric robust to images of different resolutions.
+    # 4. Quantify hair coverage
     total_pixels = image.shape[0] * image.shape[1]
     hair_pixels = cv2.countNonZero(hair_mask)
     hair_coverage_percentage = (hair_pixels / total_pixels) * 100
 
-    # 5. Classify the hair amount into the three levels based on the thresholds.
+    # 5. Classify hair amount
     hair_level = 0
     if hair_coverage_percentage >= high_threshold_pct:
-        hair_level = 2  # A lot of hair (like in img_1453.png)
+        hair_level = 2
     elif hair_coverage_percentage >= low_threshold_pct:
-        hair_level = 1  # Some hair (like in img_1487.png)
+        hair_level = 1
     else:
+        hair_level = 0
+
+    return {'hair_level': hair_level, 'hair_coverage_pct': hair_coverage_percentage}
         hair_level = 0  # Low or no hair (like in img_1443.png)
 
     return {'hair_level': hair_level, 'hair_coverage_pct': hair_coverage_percentage}
